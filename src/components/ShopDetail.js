4c00333f82ca8cbf3e1f2ca61b34ff7d17cfc92c
fahadalduaij <fahad_osama@hotmail.com>
2021-11-02 20:10:10 +0300

    trying to solve

--- src/components/ShopDetail.js
+++ src/components/ShopDetail.js
@@ -3,15 +3,17 @@ import { Redirect, useParams } from "react-router";
 import shopStore from "../stores/shopStore";
 
 const ShopDetail = () => {
-  const { shopSlug } = useParams();
-  const shop = shopStore.shops.find((shop) => shop.slug === shopSlug);
-  if (!shop) return <Redirect to="/shops" />;
-  return (
-    <div className="detail">
-      <img className="detail-img" src={shop.image} alt={shop.name} />
-      <h2 className="title">{shop.name}</h2>
-    </div>
-  );
+	const { shopSlug } = useParams();
+	const shop = shopStore.shops.find((shop) => shop.slug === shopSlug);
+	if (!shop) return <Redirect to="/shops" />;
+
+	
+	return (
+		<div className="detail">
+			<img className="detail-img" src={shop.image} alt={shop.name} />
+			<h2 className="title">{shop.name}</h2>
+		</div>
+	);
 };
 
 export default ShopDetail;
