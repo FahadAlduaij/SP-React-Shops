f1889bd597fb89fef98d5ef9de7162bc08cdc4a5
fahadalduaij <fahad_osama@hotmail.com>
2021-11-03 16:05:14 +0300

    fixing prodcutList

--- src/components/ProductModal.js
+++ src/components/ProductModal.js
@@ -1,108 +1,108 @@
 import { Button, InputGroup, FormControl, Modal } from "react-bootstrap";
 import { useState } from "react";
-import productStore from "../stores/productStore";
+import shopStore from "../stores/shopStore";
 
 function ProductModal() {
-  const [show, setShow] = useState(false);
-  const [product, setProduct] = useState({
-    name: "",
-    image: "",
-    price: 5,
-    description: "",
-    color: "black",
-    quantity: 5,
-  });
+	const [show, setShow] = useState(false);
+	const [product, setProduct] = useState({
+		name: "",
+		image: "",
+		price: 5,
+		description: "",
+		color: "black",
+		quantity: 5,
+	});
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  const handleChange = (event) =>
-    setProduct({ ...product, [event.target.name]: event.target.value });
+	const handleChange = (event) =>
+		setProduct({ ...product, [event.target.name]: event.target.value });
 
-  const handleImage = (event) =>
-    setProduct({ ...product, image: event.target.files[0] });
+	const handleImage = (event) =>
+		setProduct({ ...product, image: event.target.files[0] });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    productStore.createProduct(product);
-    handleClose();
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		shopStore.createProduct(product);
+		handleClose();
+	};
 
-  return (
-    <>
-      <Button variant="outline-dark" onClick={handleShow}>
-        New
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>
-          <form onSubmit={handleSubmit}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Name</InputGroup.Text>
-              <FormControl
-                placeholder="Your product's name"
-                name="name"
-                value={product.name}
-                type="text"
-                onChange={handleChange}
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Image</InputGroup.Text>
-              <FormControl
-                name="image"
-                type="file"
-                onChange={handleImage}
-                placeholder="Image"
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Price</InputGroup.Text>
-              <FormControl
-                name="price"
-                value={product.price}
-                type="number"
-                onChange={handleChange}
-                placeholder="لا تصير طماع"
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Description</InputGroup.Text>
-              <FormControl
-                name="description"
-                value={product.description}
-                type="text"
-                onChange={handleChange}
-                placeholder="Description"
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Color</InputGroup.Text>
-              <FormControl
-                name="color"
-                value={product.color}
-                type="text"
-                onChange={handleChange}
-                placeholder="Color"
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Quantity</InputGroup.Text>
-              <FormControl
-                name="quantity"
-                value={product.quantity}
-                type="number"
-                onChange={handleChange}
-                placeholder="Quantity"
-              />
-            </InputGroup>
-            <Button variant="outline-dark" type="submit">
-              Add Product
-            </Button>
-          </form>
-        </Modal.Body>
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<Button variant="outline-dark" onClick={handleShow}>
+				New
+			</Button>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Body>
+					<form onSubmit={handleSubmit}>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Name</InputGroup.Text>
+							<FormControl
+								placeholder="Your product's name"
+								name="name"
+								value={product.name}
+								type="text"
+								onChange={handleChange}
+							/>
+						</InputGroup>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Image</InputGroup.Text>
+							<FormControl
+								name="image"
+								type="file"
+								onChange={handleImage}
+								placeholder="Image"
+							/>
+						</InputGroup>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Price</InputGroup.Text>
+							<FormControl
+								name="price"
+								value={product.price}
+								type="number"
+								onChange={handleChange}
+								placeholder="لا تصير طماع"
+							/>
+						</InputGroup>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Description</InputGroup.Text>
+							<FormControl
+								name="description"
+								value={product.description}
+								type="text"
+								onChange={handleChange}
+								placeholder="Description"
+							/>
+						</InputGroup>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Color</InputGroup.Text>
+							<FormControl
+								name="color"
+								value={product.color}
+								type="text"
+								onChange={handleChange}
+								placeholder="Color"
+							/>
+						</InputGroup>
+						<InputGroup className="mb-3">
+							<InputGroup.Text>Quantity</InputGroup.Text>
+							<FormControl
+								name="quantity"
+								value={product.quantity}
+								type="number"
+								onChange={handleChange}
+								placeholder="Quantity"
+							/>
+						</InputGroup>
+						<Button variant="outline-dark" type="submit">
+							Add Product
+						</Button>
+					</form>
+				</Modal.Body>
+			</Modal>
+		</>
+	);
 }
 
 export default ProductModal;
--- src/components/ShopDetail.js
+++ src/components/ShopDetail.js
@@ -1,25 +1,20 @@
 import React from "react";
 import { Redirect, useParams } from "react-router";
 import shopStore from "../stores/shopStore";
-import ProductDetails from "./ProductDetails";
-import AddProduct from "./AddProduct";
+import ProductList from "./ProductList";
 
 const ShopDetail = () => {
 	const { shopSlug } = useParams();
 	const shop = shopStore.shops.find((shop) => shop.slug === shopSlug);
 	if (!shop) return <Redirect to="/shops" />;
 
-	const productDetails = shop.product.map((product) => (
-		<ProductDetails product={product} />
-	));
+
 
 	return (
 		<div className=" flex row detail m-3">
-			<AddProduct shop={shop} />
-
-			{/* <img className="detail-img" src={shop.image} alt={shop.name} /> */}
 			<h2 className="title">{shop.name}</h2>
-			{productDetails}
+			<img className="detail-img" src={shop.image} alt={shop.name} />
+			<ProductList products={shop.product} />
 		</div>
 	);
 };
